0e7201ef437b15978b6a78c767bf54a941e7a2ff
juuqyare1 <juuqyare57@gmail.com>
2023-12-22 07:41:35 -0500

    test

--- DBconfig.js
+++ DBconfig.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose")
 
 const DBconnect = ()=>{
     try {
-        mongoose.connect("mongodb+srv://juuqyare57:juuqyare57@ecommerce.mr9f3md.mongodb.net/?retryWrites=true&w=majority/dailytaskasapp")
+        mongoose.connect("mongodb+srv://juuqyare57:juuqyare57@ecommerce.mr9f3md.mongodb.net/mydailytasks?retryWrites=true&w=majority")
         console.log("database connected !");
     } catch (error) {
         console.log(error.message);
